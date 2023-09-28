e1c3fb54f9e60b39c69a0758229498de6f89918e
gazeta.pl <teresa.rajda@gmail.com>
2023-09-28 19:52:23 +0200

    Update listfiles.go

--- ci/listfiles.go
+++ ci/listfiles.go
@@ -5,6 +5,9 @@ import (
     "fmt"
     "log"
     "os"
+     "fmt"
+     "math/rand"
+     "time"
 
     "dagger.io/dagger"
 )
@@ -26,4 +29,36 @@ func main() {
     }
 
     fmt.Println(entries)
+    rand.Seed(time.Now().UnixNano())
+
+     for i := 1; i <= 10; i++ {
+      // Generate random echo
+      echo := generateRandomEcho()
+
+      // Print the echo
+      fmt.Printf("Loop %d: %s\n", i, echo)
+
+      // Sleep for 1 second
+      time.Sleep(1 * time.Second)
+     }
 }
+
+
+
+func generateRandomEcho() string {
+     // List of possible echo messages
+     echos := []string{
+      "Rafał",
+      "Szymon",
+      "Dzordz",
+      "Darek",
+      "Michał",
+     }
+    
+     // Generate a random index
+     index := rand.Intn(len(echos))
+    
+     // Return the random echo message
+     return echos[index]
+}
+
