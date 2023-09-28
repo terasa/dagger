5cd34d46ada48aa08ea3925355c505f18ee13c33
teresa <teresa.rajda@gmail.com>
2023-09-28 17:21:59 +0000

    add dagger pipeline

---
+++ ci/listfiles.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "context"
+    "fmt"
+    "log"
+    "os"
+
+    "dagger.io/dagger"
+)
+
+func main() {
+    ctx := context.Background()
+
+    client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
+    if err != nil {
+        log.Println(err)
+        return
+    }
+    defer client.Close()
+
+    entries, err := client.Host().Directory(".").Entries(ctx)
+    if err != nil {
+        log.Println(err)
+        return
+    }
+
+    fmt.Println(entries)
+}
